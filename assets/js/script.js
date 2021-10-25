38a4924cc13a44c3c5fdec43e3753efa04944056
bijink <kbijin528@gmail.com>
2021-10-25 12:46:31 +0530

    Add floating btn and home section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,36 @@
+//* Materialize jQuery *//
+
 $(document).ready(function () {
    $('.sidenav').sidenav({
       'preventScrolling': false
    });
    $('.fixed-action-btn').floatingActionButton({
-      // 'direction': 'bottom',
-      // 'hoverEnabled': false
+      'direction': 'bottom',
+      'hoverEnabled': false,
+      // 'toolbarEnabled': true,
+      // 'instance': close()
    });
 });
 
-let x = 0;
-$('#toggle').click(() => (x === 0)
-   ? `${$('.scaleClass').removeClass('scale-out').addClass('scale-in')} ${x = x -= 1}`
-   : `${$('.scaleClass').removeClass('scale-in').addClass('scale-out')} ${x = x += 1}`
-);
+//* /Materialize jQuery *//
 
 
+//* Custom jQuery
+
+// To show sideBar like  pop up
 // setTimeout(() => {
 //    $('#slide-out').addClass('sidenav-fixed');
 // }, 300);
 // setTimeout(() => {
 //    $('#slide-out').removeClass('sidenav-fixed');
 // }, 1200);
+
+
+
+// let x = 0;
+// $('#toggle').click(() => (x === 0)
+//    ? `${$('.scaleClass').removeClass('scale-out').addClass('scale-in')} ${x = x -= 1}`
+//    : `${$('.scaleClass').removeClass('scale-in').addClass('scale-out')} ${x = x += 1}`
+// );
+
+//* /Custom jQuery *//
