81de2fb980186e1bc5922154941e916faea2fee1
bijink <kbijin528@gmail.com>
2021-11-01 16:00:11 +0530

    Add about, services, works sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,17 @@ $(document).ready(function () {
       // 'toolbarEnabled': true,
       // 'instance': close()
    });
+   $('.carousel').carousel({
+      // 'numVisible': 4,
+      // 'noWrap': true,
+      'indicators': true
+   });
 });
 
 //* /Materialize jQuery *//
 
 
-//* Custom jQuery
+//* Custom jQuery *//
 
 // To show sideBar like  pop up
 // setTimeout(() => {
@@ -24,13 +29,5 @@ $(document).ready(function () {
 // setTimeout(() => {
 //    $('#slide-out').removeClass('sidenav-fixed');
 // }, 1200);
-
-
-
-// let x = 0;
-// $('#toggle').click(() => (x === 0)
-//    ? `${$('.scaleClass').removeClass('scale-out').addClass('scale-in')} ${x = x -= 1}`
-//    : `${$('.scaleClass').removeClass('scale-in').addClass('scale-out')} ${x = x += 1}`
-// );
 
 //* /Custom jQuery *//
