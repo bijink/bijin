94ce28cabf0bb4a0aace239f205e70c9ff8a4fc4
bijink <kbijin528@gmail.com>
2021-11-19 14:33:05 +0530

    Add seperate scss file for colors, modify fn for floating btn icon change

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,16 +36,37 @@ $(document).ready(function () {
 //    $('#slide-out').removeClass('sidenav-fixed');
 // }, 1200);
 
-// To hide pulse effect from toggleBtn of fixed-action-btn after user first click;
-$(".fixed-action-btn__toggle-btn").click(function () {
-   $(".fixed-action-btn__toggle-btn").removeClass('pulse');
-});
+{
+   const toggle_btn = $('.fixed-action-btn__toggle-btn');
+   const floating_btn = $('.fixed-action-btn__floating-btn > li');
+   const icon = $('.fixed-action-btn__toggle-btn > .menu-btn');
 
-// To change menu icon to close icon on toggleBtn of fixed-action-btn
-var btn = $('.fixed-action-btn__toggle-btn');
-var icon = $('.fixed-action-btn__toggle-btn > .menu-btn');
-let x = 0;
-$(btn).click(() => (x === 0)
-   ? `${$(icon).text('').text('close')} ${x = x -= 1}`
-   : `${$(icon).text('').text('menu')} ${x = x += 1}`
-);
+   let state = false;
+
+   // To change menu icon to close icon on toggleBtn of fixed-action-btn when btn clicked
+   $(toggle_btn).click(() => {
+      !state ?
+         ($(icon).text('').text('close')) && (state = true)
+         :
+         ($(icon).text('').text('menu')) && (state = false);
+   });
+   $(floating_btn).click(() => {
+      state && ($(icon).text('menu')) && (state = false);
+   });
+
+   // // To toggle pulse effect with clicks on toggleBtn of fixed-action-btn
+   // $(toggle_btn).click(() => {
+   //    state ?
+   //       $(toggle_btn).removeClass('pulse')
+   //       :
+   //       $(toggle_btn).addClass('pulse');
+   // });
+   // $(floating_btn).click(() => {
+   //    !state && $(".fixed-action-btn__toggle-btn").addClass('pulse');
+   // });
+
+   // To remove pulse effect in toggleBtn of fixed-action-btn after first click
+   $(toggle_btn).click(() => {
+      $(toggle_btn).removeClass('pulse');
+   });
+}
